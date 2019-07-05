4f178b26890b7036caa8d6af7e824ec7f907b580
OlhaKa <karazhakovao@gmail.com>
2019-07-05 15:31:23 +0300

    map, mobile, fixes

--- app/js/common.js
+++ app/js/common.js
@@ -3,44 +3,48 @@ $(document).ready(function () {
 
     $('.header__burger').click(function() {
         $(this).toggleClass('active');
-        // $('.header__navWrap').stop().slideToggle(400,
-        //     function () {
-        //         if ($(this).css("display") === "none") {
-        //             $(this).removeAttr("style");
-        //         }
-        //     });
-    });
-    var a = 0;
-    $(window).scroll(function() {
-
-        var oTop = $('.number__item__border').offset().top - window.innerHeight;
-        if (a == 0 && $(window).scrollTop() > oTop) {
-            $('.counter-value').each(function() {
-                var $this = $(this),
-                    countTo = $this.attr('data-count');
-                $({
-                    countNum: $this.text()
-                }).animate({
-                        countNum: countTo
-                    },
-
-                    {
-
-                        duration: 2500,
-                        easing: 'swing',
-                        step: function() {
-                            $this.text(Math.floor(this.countNum));
-                        },
-                        complete: function() {
-                            $this.text(this.countNum);
-                            //alert('finished');
-                        }
-
-                    });
+        $('.header__navWrap').stop().slideToggle(400,
+            function () {
+                if ($(this).css("display") === "none") {
+                    $(this).removeAttr("style");
+                }
             });
-            a = 1;
-        }
     });
+
+    // ==========HOME PAGE============
+
+    if ($(".home").length){
+        var a = 0;
+        $(window).scroll(function() {
+            var oTop = $('.number__item__border').offset().top - window.innerHeight;
+            if (a == 0 && $(window).scrollTop() > oTop) {
+                $('.counter-value').each(function() {
+                    var $this = $(this),
+                        countTo = $this.attr('data-count');
+                    $({
+                        countNum: $this.text()
+                    }).animate({
+                            countNum: countTo
+                        },
+                        {
+                            duration: 2500,
+                            easing: 'swing',
+                            step: function() {
+                                $this.text(Math.floor(this.countNum));
+                            },
+                            complete: function() {
+                                $this.text(this.countNum);
+                            }
+                        });
+                });
+                a = 1;
+            }
+        });
+    }
+
+    // ==========HOME PAGE end============
+
+
 
     function activateMap(map, frame) {
         $(map).click(function () {
@@ -69,28 +73,7 @@ $(document).ready(function () {
         $(card).hover(function () {
             $(next).toggleClass('next')
         })
-    };
-
-    $(document).delegate('.open', 'click', function(event){
-        $(this).addClass('oppenned');
-        $('.header__navWrap').stop().slideToggle(400,
-            function () {
-                if ($(this).css("display") === "none") {
-                    $(this).removeAttr("style");
-                }
-            });
-        event.stopPropagation();
-    });
-    $(document).delegate('body', 'click', function(event) {
-        $('.open').removeClass('oppenned');
-    });
-    $(document).delegate('.cls', 'click', function(event){
-        $('.open').removeClass('oppenned');
-
-
-        event.stopPropagation();
-    });
-
+    }
 
 });
 
